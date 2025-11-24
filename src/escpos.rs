ce3e4b8cac72a13af0ca2bfc1fcbf348ab7849b8
DanThomas64 <5899996+DanThomas64@users.noreply.github.com>
2025-11-24 00:58:36 -0400

    feat: Add QR code for task URL and enhance printout formatting

--- src/escpos.rs
+++ src/escpos.rs
@@ -1,27 +1,43 @@
 use crate::datatypes::Task;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use escpos::driver::FileDriver;
-use escpos::utils::{JustifyMode, Protocol};
+use escpos::utils::*;
 use escpos::printer::Printer;
+use escpos::ui::line::*;
 use std::path::Path;
+use std::env;
 
 pub fn print_task(task: &Task, device_path: &str) -> Result<()> {
     let path = Path::new(device_path);
     let driver = FileDriver::open(path)?;
     let mut printer = Printer::new(driver, Protocol::default(), None);
 
-    printer.init()?;
-    printer.justify(JustifyMode::CENTER)?;
-    print_header(&mut printer, "Task")?;
-    printer.writeln(&task.title)?;
-    printer.justify(JustifyMode::LEFT)?;
-    printer.feed()?;
-    printer.write("Project ID: ")?;
-    printer.write(&task.project_id.to_string())?;
-    printer.feeds(2)?;
-    printer.writeln("Description:")?;
-    printer.write(&task.description_as_text(42))?;
-    printer.feeds(2)?;
+    let line = LineBuilder::new().style(LineStyle::Custom("=-")).build();
+    let base_url = env::var("BASE_URL").context("API_URL environment variable not set")?;
+    let task_url = format!("{base_url}/tasks/{}", task.id);
+
+    printer.init()?
+        .justify(JustifyMode::CENTER)?
+        .qrcode_option(
+                task_url.as_str(),
+                QRCodeOption::new(QRCodeModel::Model1, 6, QRCodeCorrectionLevel::M),
+        )?;
+    print_header(&mut printer, "--- Task ---")?;
+    printer.bold(true)?
+        .writeln(&task.title)?
+        .feed()?
+        .draw_line(line)?
+        .bold(false)?
+        .reset_size()?
+        .justify(JustifyMode::LEFT)?
+        .feed()?
+        .writeln("Description:")?
+        .write(&task.description_as_text(42))?
+        .feeds(2)?
+        .write("Due Date:")?
+        .feed()?
+        .write(&task.due_date.to_string())?
+        .feeds(2)?;
 
     if let Some(labels) = &task.labels {
         if !labels.is_empty() {
@@ -32,22 +48,26 @@ pub fn print_task(task: &Task, device_path: &str) -> Result<()> {
         }
     }
 
-    print_footer(&mut printer, "Task")?;
-    printer.cut()?;
+    print_footer(&mut printer, "--- End Task ---")?;
+    printer.print_cut()?;
     Ok(())
 }
-
 fn print_header(printer: &mut Printer<FileDriver>, print_type: &str) -> Result<()> {
-    printer.feeds(4)?;
-    printer.write(print_type)?;
-    printer.feeds(4)?;
+
+
+    printer.feeds(2)?
+        .size(2,2)?
+        .write(print_type)?
+        .feeds(2)?
+        .feeds(2)?;
     Ok(())
 }
 
 fn print_footer(printer: &mut Printer<FileDriver>, print_type: &str) -> Result<()> {
-    printer.justify(JustifyMode::CENTER)?;
-    printer.feeds(4)?;
-    printer.write(print_type)?;
-    printer.feeds(9)?;
+    printer.justify(JustifyMode::CENTER)?
+        .feeds(2)?
+        .size(2,2)?
+        .write(print_type)?
+        .feeds(6)?;
     Ok(())
 }
